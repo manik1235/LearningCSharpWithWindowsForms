8dc9a51554c3c98efb671b2960c225413d45b25a
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2017-09-28 22:58:19 -0400

    Add project files.

---
+++ LearningCSharpWithWindowsForms/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace LearningCSharpWithWindowsForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.helloButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // helloButton
+            // 
+            this.helloButton.Location = new System.Drawing.Point(52, 36);
+            this.helloButton.Name = "helloButton";
+            this.helloButton.Size = new System.Drawing.Size(136, 41);
+            this.helloButton.TabIndex = 0;
+            this.helloButton.Text = "Hello";
+            this.helloButton.UseVisualStyleBackColor = true;
+            this.helloButton.Click += new System.EventHandler(this.HelloButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(207, 47);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(136, 20);
+            this.nameTextBox.TabIndex = 1;
+            this.nameTextBox.Text = "World";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 248);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.helloButton);
+            this.Name = "MainForm";
+            this.Text = "Hello... Someone";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button helloButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+    }
+}
+
---
+++ LearningCSharpWithWindowsForms/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LearningCSharpWithWindowsForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        ///   When this button is clicked, it displays a message box saying hello to whomever is listed in the associated text box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HelloButton_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Hello " & )
+        }
+
+
+    }
+}
